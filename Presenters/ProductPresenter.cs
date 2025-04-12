739b83cb281869af80f7041a1e89f02bf182ec5c
Alejandro Martinez Cisnero <ale.marcho0514@gmail.com>
2025-04-11 22:42:20 -0500

    ProductPresenter

---
+++ Presenters/ProductPresenter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket_mvp.Models;
+using Supermarket_mvp.Views;
+
+namespace Supermarket_mvp.Presenters
+{
+    internal class ProductPresenter
+    {
+        private IProductView view;
+        private IProductRepository repository;
+        private BindingSource productBindingSource;
+        private IEnumerable<ProductModel> productList;
+        public ProductPresenter(IProductView view, IProductRepository repository)
+        {
+            this.productBindingSource = new BindingSource();
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchProduct;
+            this.view.AddNewEvent += AddNewProduct;
+            this.view.EditEvent += LoadSelectProductToEdit;
+            this.view.DeleteEvent += DeleteSelectedProduct;
+            this.view.SaveEvent += SaveProduct;
+            this.view.CancelEvent += CancelProduct;
+
+        }
+    }
+}
