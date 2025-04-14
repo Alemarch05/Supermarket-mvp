6c26ec6ca6a82cc2008050bb6cbe635e8cf200ca
Alejandro Martinez Cisnero <ale.marcho0514@gmail.com>
2025-04-13 21:28:44 -0500

    CustomerPresenter

---
+++ Presenters/CustomerPresenter.cs
@@ -0,0 +1,96 @@
+﻿using Supermarket_mvp.Models;
+using Supermarket_mvp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket_mvp.Models;
+using Supermarket_mvp.Views;
+
+namespace Supermarket_mvp.Presenters
+{
+    internal class CustomerPresenter
+    {
+        private ICustomerView view;
+        private ICustomerRepository repository;
+        private BindingSource customerBindingSource;
+        private IEnumerable<CustomerModel> customerList;
+
+        public CustomerPresenter(ICustomerView view, ICustomerRepository repository)
+        {
+            this.customerBindingSource = new BindingSource();
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchCustomer;
+            this.view.AddNewEvent += AddNewCustomer;
+            this.view.EditEvent += LoadSelectCustomerToEdit;
+            this.view.DeleteEvent += DeleteSelectedCustomer;
+            this.view.SaveEvent += SaveCustomer;
+            this.view.CancelEvent += CancelCustomer;
+
+            this.view.SetCustomerBindingSource(customerBindingSource);
+
+
+
+            LoadAllCustomerList();
+
+            this.view.Show();
+        }
+
+        private void LoadAllCustomerList()
+        {
+            customerList = repository.GetAll();
+            customerBindingSource.DataSource = customerList;
+        }
+
+        private void SaveCustomer(object? sender, EventArgs e)
+        {
+           
+        }
+        private void CleanViewFields()
+        {
+            
+        }
+
+        private void CancelAction(object? sender, EventArgs e)
+        {
+            CleanViewFields();
+        }
+        private void CancelCustomer(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadSelectCustomerToEdit(object? sender, EventArgs e)
+        {
+         
+        }
+
+        private void DeleteSelectedCustomer(object? sender, EventArgs e)
+        {
+         
+        }
+
+        private void AddNewCustomer(object? sender, EventArgs e)
+        {
+            view.IsEdit = false;
+        }
+
+        private void SearchCustomer(object? sender, EventArgs e)
+        {
+            bool emptyValue = string.IsNullOrWhiteSpace(this.view.SearchValue);
+            if (emptyValue == false)
+            {
+                customerList = repository.GetByValue(this.view.SearchValue);
+            }
+            else
+            {
+                customerList = repository.GetAll();
+            }
+            customerBindingSource.DataSource = customerList;
+
+        }
+    }
+}
